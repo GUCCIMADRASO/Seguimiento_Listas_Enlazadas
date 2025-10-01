a43878c3d01b93c4d59db5ccf31f8703236359c6
IsabelaOspina <isabela.ospinab@uqvirtual.edu.co>
2025-10-01 08:34:14 -0500

    feat: Pila

---
+++ SeguimientoListasEnlazadas/src/main/java/org/example/Pila/Pila.java
@@ -0,0 +1,32 @@
+package org.example.Pila;
+
+public class Pila <E>{
+    private NodoPila<E> cima;
+    private int tam;
+
+    public Pila() {
+        cima = null;
+        tam = 0;
+    }
+
+    public void apilar(E dato) {
+        NodoPila<E> nuevo = new NodoPila<>(dato);
+        nuevo.setSiguiente(cima);
+        cima = nuevo;
+        tam++;
+    }
+
+    public E desapilar() {
+        if (estaVacia()) {
+            throw new RuntimeException("La pila está vacía");
+        }
+        E valor = cima.getDato();
+        cima = cima.getSiguiente(); // la cima pasa al siguiente nodo
+        tam--;
+        return valor;
+    }
+
+    public boolean estaVacia() {
+        return cima == null;
+    }
+}
